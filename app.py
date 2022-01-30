3401454fd896503dd83ba86c7c0ec01a800b03c1
aa-ag <aaron.aguerrevere@gmail.com>
2022-01-30 11:38:11 -0600

    Add app/Flask object instantiation

--- app.py
+++ app.py
@@ -2,5 +2,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 ### GLOBAL
+# instatiate the Flask app/object
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 ### ROUTES
